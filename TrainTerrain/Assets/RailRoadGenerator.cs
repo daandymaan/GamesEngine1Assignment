af14d5637601045c4e72571d8741281f64af8731
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2020-12-23 12:08:19 +0000

    Created intial code for railroad generation

--- TrainTerrain/Assets/RailRoadGenerator.cs
+++ TrainTerrain/Assets/RailRoadGenerator.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class RailRoadGenerator : MonoBehaviour
 {
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateShape()
     {
-        
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+        };
+    
+        triangles = new int[]
+        {
+            0, 1, 2
+        };
     }
+
+    void updateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles; 
+
+        mesh.RecalculateNormals();
+    }
+
 }
