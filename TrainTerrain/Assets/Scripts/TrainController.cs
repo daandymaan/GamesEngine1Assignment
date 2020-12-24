4d29dcc8671b8ae4c240bca2e2ac104cd50fb4be
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2020-12-24 16:17:41 +0000

    Created train controller class to improvements to how the train is controlled, implemented a start button 'w' to start the trains movement

---
+++ TrainTerrain/Assets/Scripts/TrainController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour
+{
+    public float speed = 10;
+    bool startTrain = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            startTrain = true;
+        }
+
+        if(startTrain)
+        {
+            Vector3 targetPos = transform.position;
+            targetPos += (Vector3.forward * speed * Time.deltaTime);
+            transform.position = targetPos;
+        }
+
+        
+    }
+}
